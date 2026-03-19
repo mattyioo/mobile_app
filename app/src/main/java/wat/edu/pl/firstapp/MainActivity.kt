75de29f3ce40f4946570dd226cf6ba56d4994d53
mattyioo <mefu2232@gmail.com>
2026-03-19 09:59:31 +0100

    Fix some warnings

--- app/src/main/java/wat/edu/pl/firstapp/MainActivity.kt
+++ app/src/main/java/wat/edu/pl/firstapp/MainActivity.kt
@@ -1,8 +1,5 @@
 package wat.edu.pl.firstapp
 
-import android.R.attr.label
-import android.R.attr.value
-import android.R.attr.width
 import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
@@ -13,40 +10,32 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import wat.edu.pl.firstapp.ui.theme.FirstAppTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -55,32 +44,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationApp()
-
+//            MainApp()
         }
     }
 }
 
 
 
-private fun pop_up(context: Context){
+private fun popUp(context: Context){
     Toast.makeText(context, "Przycisk zostal klikniety.", Toast.LENGTH_SHORT).show()
 }
 
 @Composable
-fun main_app() {
+fun MainApp() {
     val context = LocalContext.current
-    var number by rememberSaveable() { mutableStateOf(0) }
-    var text by rememberSaveable() { mutableStateOf("") }
-    var text1 by rememberSaveable() { mutableStateOf("") }
-    val list=rememberSaveable() { mutableStateListOf<String>() }
-    val mod=Modifier.padding(5.dp)
+    var number by rememberSaveable { mutableIntStateOf(0) }
+    var text by rememberSaveable { mutableStateOf("") }
+    var text1 by rememberSaveable { mutableStateOf("") }
+    val list = rememberSaveable { mutableStateListOf<String>() }
+    val mod = Modifier.padding(5.dp)
     Column(
         modifier = Modifier.padding(24.dp).fillMaxSize().paddingFromBaseline(top = 50.dp)
     ) {
         Text(
             text = "Witaj w JetpackCompose!", modifier = mod
         )
-        FilledTonalButton( onClick = { pop_up(context) }, modifier = mod)
+        FilledTonalButton( onClick = { popUp(context) }, modifier = mod)
         {
             Text(text = "Kliknij mnie", modifier = mod) // Button content (text only)
         }
@@ -124,7 +113,7 @@ fun main_app() {
             itemsIndexed(items = list) {index, flower ->
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Text(text = flower, modifier = mod)
-                    FilledTonalButton(onClick ={list.removeAt(index)} , modifier = mod,) {
+                    FilledTonalButton(onClick ={list.removeAt(index)} , modifier = mod) {
                         Text("Usuń")
                     }
                 }
@@ -194,4 +183,3 @@ fun SecondScreen( onBack: () -> Unit) {
         }
     }
 }
-
