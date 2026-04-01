9ca28557691216d2495c7f503573930c1420a254
mattyioo <mefu2232@gmail.com>
2026-04-01 18:08:32 +0200

    Add ToDoRepository file

---
+++ app/src/main/java/wat/edu/pl/firstapp/ToDoRepository.kt
@@ -0,0 +1,19 @@
+package wat.edu.pl.firstapp
+
+import kotlinx.coroutines.flow.Flow
+
+class TodoRepository(
+    private val todoDao: TodoDao
+) {
+    val todos : Flow<List<TodoEntity>> =todoDao.getAllTodos()
+    suspend fun addTodo(title: String){
+        val todo= TodoEntity(
+            title=title,
+            createdAt = System.currentTimeMillis()
+        )
+        todoDao.insert(todo)
+    }
+    suspend fun removeTodo(todo: TodoEntity){
+        todoDao.delete(todo)
+    }
+}
