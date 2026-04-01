a4e76a680d463adb1875bdf5b234aa1f32204aa1
mattyioo <mefu2232@gmail.com>
2026-04-01 18:13:30 +0200

    Add AppDatabase file

---
+++ app/src/main/java/wat/edu/pl/firstapp/AppDatabase.kt
@@ -0,0 +1,27 @@
+package wat.edu.pl.firstapp
+
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [TodoEntity::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun todoDao(): TodoDao
+    companion object{
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        fun getDatabase (context: Context): AppDatabase{
+            return INSTANCE?:synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE=instance
+                instance
+            }
+        }
+    }
+}
