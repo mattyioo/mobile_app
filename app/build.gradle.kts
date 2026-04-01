3d654ef4a3df9b801e29502e9fa2c13879333435
mattyioo <mefu2232@gmail.com>
2026-04-01 18:07:41 +0200

    Add ToDoViewModel file and change gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,6 +41,9 @@ android {
 }
 
 dependencies {
+    ksp("androidx.room:room-compiler:2.8.4")
+    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/wat/edu/pl/firstapp/ToDoViewModel.kt
@@ -0,0 +1,22 @@
+package wat.edu.pl.firstapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class TodoViewModel(
+    private val repository: TodoRepository
+) : ViewModel(){
+    val todos: Flow<List<TodoEntity>> = repository.todos
+    fun add(title: String){
+        viewModelScope.launch {
+            repository.addTodo(title)
+        }
+    }
+    fun delete(todo: TodoEntity){
+        viewModelScope.launch{
+            repository.removeTodo(todo)
+        }
+    }
+}
