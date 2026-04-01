a65d0938e34a2dd9f68f27b46a00066113285d84
mattyioo <mefu2232@gmail.com>
2026-04-01 18:44:24 +0200

    Change AppNavHost func

--- app/src/main/java/wat/edu/pl/firstapp/MainActivity.kt
+++ app/src/main/java/wat/edu/pl/firstapp/MainActivity.kt
@@ -358,23 +358,16 @@ enum class Destination(
 fun AppNavHost(
     navController: NavHostController,
     startDestination: Destination,
-    modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController,
-        startDestination = startDestination.route
-    ) {
-        Destination.entries.forEach { destination ->
-            composable(destination.route) {
-                when (destination) {
-                    Destination.Home -> HomeScreen()
-                    Destination.List -> ListScreen()
-                    Destination.Settings -> SettingsScreen()
-                }
-            }
-        }
+    modifier: Modifier=Modifier,
+    todoViewModel: TodoViewModel
+){
+    NavHost(navController,startDestination.route,modifier){
+        composable(Destination.Home.route){HomeScreen()}
+        composable(Destination.List.route){ListScreen(todoViewModel)}
+        composable(Destination.Settings.route){SettingsScreen()}
     }
 }
+
 
 @Composable
 fun MainScreen(todoViewModel: TodoViewModel){
