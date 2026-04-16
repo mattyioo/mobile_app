4a3c5cb26375ae673c7d7395e6607bc4523cf71a
mattyioo <mefu2232@gmail.com>
2026-04-16 21:38:16 +0200

    Add refresh function which takes data from API in MainViewModel file

--- app/src/main/java/wat/edu/pl/firstapp/MainViewModel.kt
+++ app/src/main/java/wat/edu/pl/firstapp/MainViewModel.kt
@@ -6,8 +6,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel(){
+class MainViewModel(private val repository: Repository = Repository()) : ViewModel(){
     var counter by mutableStateOf(0)
     var name by mutableStateOf("")
     var newItemText by mutableStateOf("")
@@ -30,5 +32,29 @@ class MainViewModel : ViewModel(){
     }
     fun removeitem(item : String){
         itemsList.remove(item)
+    }
+
+    //Retrofit zadanie 4
+    var loading by mutableStateOf(false)
+        private set
+
+    var error by mutableStateOf<String?>(null)
+        private set
+
+    var data by mutableStateOf<List<PostDto>>(emptyList())
+        private set
+
+    fun refresh() {
+        viewModelScope.launch {
+            loading = true
+            error = null
+            try {
+                data = repository.getData()
+            } catch (e: Exception) {
+                error = e.message ?: "Unknown error"
+            } finally {
+                loading = false
+            }
+        }
     }
 }
