6512cfda125a77948f2990d9a14399ea97933f59
mattyioo <mefu2232@gmail.com>
2026-04-16 21:33:18 +0200

    Add Repository class which gets data from API

---
+++ app/src/main/java/wat/edu/pl/firstapp/Repository.kt
@@ -0,0 +1,10 @@
+package wat.edu.pl.firstapp
+
+class Repository(
+    private val api: APIInterface = RetrofitProvider.api
+) {
+
+    suspend fun getData(): List<PostDto> {
+        return api.getPosts()
+    }
+}
