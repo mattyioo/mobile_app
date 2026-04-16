5501c01c5c58f3717a82731d7cf6beeb5e122620
mattyioo <mefu2232@gmail.com>
2026-04-16 21:29:29 +0200

    Add Retrofit object file

---
+++ app/src/main/java/wat/edu/pl/firstapp/RetrofitProvider.kt
@@ -0,0 +1,36 @@
+package wat.edu.pl.firstapp
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitProvider {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    val api: APIInterface by lazy {
+        retrofit.create(APIInterface::class.java)
+    }
+}
